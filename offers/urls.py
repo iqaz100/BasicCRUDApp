4c55a8ddb07095e6dbec56b47e1715b029e2acfd
Piotr Łasek <piotrek10.lasek@o2.pl>
2021-12-05 22:55:26 +0100

    Version 1.0

---
+++ offers/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+app_name = 'offers'
+
+urlpatterns = [
+    path('offers/', OfferList.as_view(), name='offers'),
+    path('offers/<int:pk>/', OfferDetail.as_view(), name='offerdetail'),
+    path('categories/', CategoryList.as_view(), name='categories'),
+    path('categories/<int:pk>/', CategoryDetail.as_view(), name='categorydetail'),
+]
--- readme.txt
+++ readme.txt
@@ -4,5 +4,3 @@ Additionally:
 I used serializers and models to data validation.
 I used ,,generics" from DRF to implement Views/Endpoints.
 I covered models and endpoints with basics tests
-
-All the plugins that I used are in requirements.txt file.
